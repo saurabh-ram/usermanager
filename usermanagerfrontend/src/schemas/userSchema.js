89aa1b44ae0b4ef44fed1374df2090c678a0722c
Saurabh Ram <saurabhram37@gmail.com>
2025-11-14 08:05:23 +0530

    Schema updated

--- usermanagerfrontend/src/schemas/userSchema.js
+++ usermanagerfrontend/src/schemas/userSchema.js
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const userSchema = z.object({
-  name: z.string().min(3, "Name must be at least 3 characters"),
-  email: z.string().email("Invalid email"),
-  age: z.number().min(1, "Age must be > 0"),
+  name: z.string().min(2, "Name must be at least 2 characters"),
+  email: z.email(),
+  age: z.number().min(18, "Age must be > 17"),
 });
